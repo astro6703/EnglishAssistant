ce6a26b0ae2e0b28257c575d63bfbf1d569c04cc
Kirill Gorbenko <astro6703@gmail.com>
2020-04-12 15:59:15 +0300

    Make css bundles separate, use file-loader

--- EnglishAssistant/ClientApp/webpack.config.js
+++ EnglishAssistant/ClientApp/webpack.config.js
@@ -1,15 +1,17 @@
 ﻿const path = require('path');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'none',
     entry: {
-        index: './src/home/index.ts'
+        'home/index': './src/home/index.ts'
     },
     devtool: 'inline-source-map',
     module: {
         rules: [
-            { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
-            { test: /\.s[ac]ss$/i, use: [ 'style-loader', 'css-loader', 'sass-loader' ] }
+            { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/ },
+            { test: /\.s[ac]ss$/i, use: [ miniCssExtractPlugin.loader, 'css-loader', 'resolve-url-loader', 'sass-loader' ] },
+            { test: /\.(png|jpe?g|gif)/i, loader: 'file-loader', options: { name: '[path][name].[ext]', outputPath: 'images' } },
         ],
     },
     resolve: {
@@ -17,6 +19,9 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'bundles'),
-        filename: '[name].bundle.js'
-    }
+        filename: '[name].bundle.js',
+    },
+    plugins: [
+        new miniCssExtractPlugin({ filename: "[name].bundle.css" }),
+    ]
 };
