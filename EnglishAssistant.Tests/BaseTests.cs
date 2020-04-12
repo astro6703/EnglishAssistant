390fe2643dd4b65e2b9803bd6ee33efdf1379e03
Kirill Gorbenko <astro6703@gmail.com>
2020-04-12 21:30:13 +0300

    Create test project

---
+++ EnglishAssistant.Tests/BaseTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace EnglishAssistant.Tests
+{
+    [TestFixture]
+    public class BaseTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
