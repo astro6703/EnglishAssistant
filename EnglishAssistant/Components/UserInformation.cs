bfaddc678fb3087fe7d3c37cec8f4becb70a36b6
Kirill Gorbenko <astro6703@gmail.com>
2020-05-21 00:24:52 +0300

    Introduce UserInformationViewModel

---
+++ EnglishAssistant/Components/UserInformation.cs
@@ -0,0 +1,21 @@
+﻿using EnglishAssistant.ViewModels.Shared.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishAssistant.Components
+{
+    public class UserInformation : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var userIdentity = User.Identity;
+            var isAuthenticated = userIdentity?.IsAuthenticated ?? false;
+            var viewModel = new UserInformationViewModel
+            {
+                IsAuthenticated = isAuthenticated,
+                Username = isAuthenticated ? userIdentity.Name : string.Empty
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ EnglishAssistant/ViewModels/Shared/Components/UserInformationViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EnglishAssistant.ViewModels.Shared.Components
+{
+    public class UserInformationViewModel
+    {
+        public bool IsAuthenticated { get; set; }
+        public string Username { get; set; }
+    }
+}
